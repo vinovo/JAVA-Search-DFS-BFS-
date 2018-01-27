856d3d745e3fb4548a579c75364c4a4b17904d68
demonikki <nsankara@ucsd.edu>
2018-01-26 17:32:02 -0800

    refactoring

--- ucsd-cse12-pa3-student/src/cse12pa3student/TestSolvers.java
+++ ucsd-cse12-pa3-student/src/cse12pa3student/TestSolvers.java
@@ -9,8 +9,32 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestSolvers {
+
+
+	/* Helper method to compare two mazes */
+	public void checkMaze(SearchWorklist wl, Maze startMaze, String[] expected) {
+		Square s = MazeSolver.solve(startMaze, new StackWorklist());
+		if(expected == null) { assertNull(s); }
+		else {
+			String actualStr = formatMaze(startMaze.showSolution());
+			String expectedStr = formatMaze(expected);
+			assertEquals(expectedStr, actualStr);
+		}
+	}
+	
+
+	/* Helper method to format String[] output as String */
+	public String formatMaze(String[] arr) {
+		String result = "";
+		for (String s: arr)
+			result += "\n"+s;
+		return (result+"\n");
+	}
+
+	
 	
 	/* Worklist Tests */
+	
 	@Test
 	public void testStackWorklist() {
 		Square s1 = new Square(0,0, false);
@@ -26,7 +50,7 @@ public class TestSolvers {
 		swl.remove();
 		assertEquals(true, swl.isEmpty());
 		
-		//Ensure that behaves like a stack
+		//Ensure stack behavior
 		swl.add(s1);
 		swl.add(s2);
 		swl.add(s3);
@@ -49,22 +73,13 @@ public class TestSolvers {
 		qwl.remove();
 		assertEquals(true, qwl.isEmpty());
 		
+		//Ensure queue behavior
 		qwl.add(s1);
 		qwl.add(s2);
 		qwl.add(s3);
 		assertEquals(s1, qwl.remove());
 	}	
 	
-	@Test
-	public void testHelper(SearchWorklist wl, Maze startMaze, String[] expected) {
-		Square s = MazeSolver.solve(startMaze, new StackWorklist());
-		if(expected == null) { assertNull(s); }
-		else {
-			String actualStr = formatMaze(startMaze.showSolution());
-			String expectedStr = formatMaze(expected);
-			assertEquals(expectedStr, actualStr);
-		}
-	}
 	
 	@Test
 	public void testClassExample() {
@@ -80,7 +95,7 @@ public class TestSolvers {
 				"*##S",
 				"F___"
 			};
-		testHelper(new StackWorklist(), m, stackExpected);
+		checkMaze(new StackWorklist(), m, stackExpected);
 	}
 	
 	@Test
@@ -98,14 +113,6 @@ public class TestSolvers {
 				"F***"
 			};
 		assertEquals(formatMaze(expected), formatMaze(actualAndWrong));
-	}
-
-	/* Helper method to format String[] output as String */
-	public String formatMaze(String[] arr) {
-		String result = "";
-		for (String s: arr)
-			result += "\n"+s;
-		return (result+"\n");
 	}
 }
 
